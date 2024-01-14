f4e58fd86e8c4a2a709ca097be2eefc3e4d8b5a3
Mathumitha.M <mmathumitha@appen.com>
2024-01-14 14:40:11 +0530

    changes made in destination of studentDaoImpl since was not available for component scannning

---
+++ src/main/java/com/example/CRUDdemo/StudentDaoImpl.java
@@ -0,0 +1,25 @@
+import com.example.CRUDdemo.entity.Student;
+import com.example.CRUDdemo.studentDao;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDaoImpl implements studentDao {
+    private EntityManager entityManager;
+    @Autowired
+    public StudentDaoImpl(EntityManager em){
+        this.entityManager=em;
+    }
+    @Override
+    @Transactional
+    public void save(Student theStudent){
+
+        entityManager.persist(theStudent);
+
+
+
+    }
+
+}
