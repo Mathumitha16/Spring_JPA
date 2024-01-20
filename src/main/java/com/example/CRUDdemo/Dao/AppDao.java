395d96c025bca5620ee825991f2f8cf1190893c9
Mathumitha.M <mmathumitha@appen.com>
2024-01-20 21:42:01 +0530

    findById and DeleteById implemented for OneToOne unidirectional relationship.

--- src/main/java/com/example/CRUDdemo/Dao/AppDao.java
+++ src/main/java/com/example/CRUDdemo/Dao/AppDao.java
@@ -4,4 +4,7 @@ import com.example.CRUDdemo.entity.Instructor;
 
 public interface AppDao {
     public void saveInstructor(Instructor theInstructor);
+    public Instructor findInstructorById(int id);
+
+    public void deleteInstructor(int id);
 }
